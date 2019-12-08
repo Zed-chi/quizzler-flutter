bc0665884368830f785f1790bae51ffd8cf8a5a4
zed <zed@mail.ru>
2019-12-08 17:53:49 +0300

    finished app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import "./quiz_brain.dart";
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,34 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int counter = 0;
+  List<Widget> scoreKeeper = [];
+
+  void nextQuestion(){
+    if (counter < quizBrain.length()-1){
+      counter+=1;
+    } else{
+      Alert(context: context, title: "You ve Answered all the questions", desc: "Resetting questionis").show();
+      counter = 0;
+      scoreKeeper.clear();
+    }
+  }
+  void markAnswer(chosing){
+    if (chosing == quizBrain.getAnswer(counter)){
+      setState(() {
+        scoreKeeper.add(
+          Icon(Icons.check, color:Colors.green)
+        );
+      });
+    } else {
+      setState(() {
+        scoreKeeper.add(
+          Icon(Icons.close, color:Colors.red)
+        );
+      });
+    }
+    nextQuestion();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +69,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getText(counter),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -47,6 +79,7 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
+        
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -61,11 +94,12 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                markAnswer(true);                                
               },
             ),
           ),
         ),
+        
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
@@ -79,19 +113,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                markAnswer(false);   
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+
+        Row(
+          children: scoreKeeper,
+        )            
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
---
+++ lib/question.dart
@@ -0,0 +1,10 @@
+class Question {
+  String text;
+  bool answer;
+
+  Question(String q, bool a){
+    text = q;
+    answer = a;
+
+  }
+}
